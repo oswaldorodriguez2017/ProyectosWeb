8aa45fe42e2a48968470d90a7b68e2288c1fd8aa
Alumno 2018 <oswaldorodriguez2017.27@gmail.com>
2018-09-12 17:56:52 -0600

    ingreso de tarea nuevas

--- Segundo Parcial/Javascript/S9/C2/controles.js
+++ Segundo Parcial/Javascript/S9/C2/controles.js
@@ -1,77 +1,18 @@
 
-// console.log("Hola desde java script ");
 
-//var x = true;
-//var y = false;
+alert("Bienvenidos al JS");
 
-//if(x){
+//var user = prompt("Ingrese Su nombre");
 
-//}
-//else {
+//alert("Bienvenido" + user + " al universo del Front End (suerte y que la fuerza te acompañe)");
 
-//}
-
-//var numeros_pares = [2, 4, 6, 8, 10];
-
-//console.log(numeros_pares);
-
-//console.log(numeros_pares[3]);
+//var centimetro = prompt("Ingrese los valores en centimetros");
 
 
-var frutas = ['pera', 'manzana', 'piña','Melon'];
+//var resultado = 0;
 
-//console.log(frutas[1]);
+//resultado = centimetro * 2.54;
 
-//console.log(frutas.length);
+//alert("El resultado convertido en pulgadas es " + resultado);
 
-// for(var i =0;i < frutas.length;i++)
-//{
-//  console.log("Fruta #" + i + " " + frutas[i]);
-// }
-
-// remove
-// push
-
-//frutas.splice(0, 3);
-
-//console.log(frutas);
-
-//frutas.push("Mango");
-
-//console.log(frutas);
-
-var frutas = {
-    nombre: "Piña",
-    sabor: "acido",
-    peso: 1
-};
-
-//var canasta = {
-//   precio: 100,
-//   frutas: [fruta]
-//};
-
-var canasta = {
-   precio: 100,
-   frutas: [
-        {
-          nombre: "Limon",
-          sabor: "acido",
-          peso: 1
-        }
-        ,
-        {
-          nombre: "Pera",
-          sabor: "Dulce",
-          peso: 2
-        }
-   ]
-};
-
-console.log(canasta);
-
-console.log(suma(5,10));
-
-function suma(x, y){
-   return x + y;
-};
+//var calificacion = prompt("Ingrese una calificacion");
